1a6d40dc9c4dee3d016e8e956231c0a43ae9170a
rdwic0407-lab <rdwic.0407@gmail.com>
2026-06-14 20:40:14 +0700

    Update InputError.jsx

--- resources/js/Components/InputError.jsx
+++ resources/js/Components/InputError.jsx
@@ -1,8 +1,12 @@
-export default function InputError({ message, className = '', ...props }) 
+// Komponen untuk menampilkan pesan error pada form
+export default function InputError({ message, className = '', ...props }) {
+
+    // Jika terdapat pesan error, maka tampilkan teks error
+    // Jika tidak ada pesan error, maka tidak menampilkan apa pun (null)
     return message ? (
         <p
-            {...props
-            className={'text-sm text-red-600 ' + className
+            {...props}
+            className={'text-sm text-red-600 ' + className}
         >
             {message}
         </p>
