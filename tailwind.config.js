411daed899ce67effc96b9ac318a90245bbfd470
rdwic0407-lab <rdwic.0407@gmail.com>
2026-06-14 18:53:56 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+// Import default theme bawaan Tailwind
 import defaultTheme from 'tailwindcss/defaultTheme';
+
+// Import plugin form agar styling form lebih bagus
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+
+    // Menentukan file yang akan dipindai Tailwind
+    // supaya class CSS yang dipakai bisa ter-generate
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
@@ -12,11 +18,16 @@ export default {
 
     theme: {
         extend: {
+
+            // Menambahkan font Figtree sebagai font utama
+            // lalu fallback ke font bawaan Tailwind
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+
         },
     },
 
+    // Plugin tambahan untuk styling form
     plugins: [forms],
 };
