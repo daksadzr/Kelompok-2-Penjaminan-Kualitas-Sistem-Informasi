7aa5272cdcd3d9a23ae41cd4bfa9d16ed8f303c0
Daksadzr <100257886+daksadzr@users.noreply.github.com>
2026-06-14 17:27:07 +0700

    Update NavLink.jsx

--- resources/js/Components/NavLink.jsx
+++ resources/js/Components/NavLink.jsx
@@ -8,8 +8,8 @@ export default function NavLink({
 }) {
     return (
         <Link
-            {...props}
-            className={
+            {...props
+            className=
                 'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium leading-5 transition duration-150 ease-in-out focus:outline-none ' +
                 (active
                     ? 'border-indigo-400 text-gray-900 focus:border-indigo-700'
